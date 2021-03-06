8e6021aff3669763623ba3135fef91a662248d9f
Никита Целков <nikitacelkov@gmail.com>
2021-03-06 10:45:00 +0700

    Закончена работа над главным окном

---
+++ MediaPlayer/UserControls/Sticks.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Timers;
+using System.Windows.Controls;
+
+
+namespace MediaPlayer.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для Sticks.xaml
+    /// </summary>
+    public partial class Sticks : UserControl
+    {
+        public Sticks()
+        {
+            InitializeComponent();
+
+            Thread thread = new Thread(() =>
+            {
+                this.Dispatcher.Invoke(new Action(() => l1.IsEnabled = true));
+
+                Thread.Sleep(100);
+
+                this.Dispatcher.Invoke(new Action(() => l3.IsEnabled = true));
+
+                Thread.Sleep(50);
+
+                this.Dispatcher.Invoke(new Action(() => l2.IsEnabled = true));
+            });
+
+            thread.Start();
+        }
+    }
+}
